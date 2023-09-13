5c47baa1b48d24ddbbe68fe395d07b3bb34cb37a
AzizAzizli <azizliaziz025@gmail.com>
2023-09-13 21:49:09 +0400

    I changed design of app and finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,49 @@
+$(document).ready(function() {
+
+window.onload = async function () {
+    try{
+        await getData("Baku")
+        render()
+    }catch(err){
+        // console.log(err);
+    }
+ 
+  };
 
 
+    $("input").on("keydown",  function(e){
+        try{
+            if(e.key=="Enter"){
+                getData($("input").val().trim())
+                $("input").val("")
+             render()
+            }
+        }catch(err){
+            // console.log(err);
+        }
+            
+        })
 
 
 function convertToUpperCase() {
     $("input").val($("input").val().toUpperCase());
 }
-$("button").on("click", async function(){
-    getData($("input").val()  )
-    $("input").val("")
-    await render()
+
+    $("input").on('input', convertToUpperCase);
+
+
+$("img").on("click", async function(){
+    try{
+        getData($("input").val().trim() )
+        $("input").val("")
+     await render()
+    }catch(err){
+        // console.log(err);
+    }
+    
+
 })
+
  async function getData(city){
     try{
         let respons=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=addeebf98ad3829186f35c693f6fa4d0&units=metric&lang=en`)
@@ -22,18 +56,42 @@ $("button").on("click", async function(){
         return   weather
 
     } catch(error){
-
+// console.log(error);
     }
 
  }
 
 function render(weather){
- const {weather:[{main,description}],main:{temp},wind:{speed} ,sys:{country},name } = weather
-console.log(main,description,temp,speed,country,name);
-$(".content").html(`<p class="h1 fw-bold">${name}/${country}</p>
-<p class="h1 fw-medium">${temp} C°</p>
-<p class="h2 ">${main}</p>
-<p class="h5 "> Wind speed: ${speed} m/s</p>`)
+    // console.log(weather.cod);
+    if(weather.cod=="404"){
+        // $("#errorMessage").removeClass("d-none")
+      
+        // $(".aboutWeather").addClass("d-none")
+        $(".aboutWeather").fadeOut("slow");
+        setTimeout(function(){
+        $("#errorMessage").fadeIn("slow")
+         },500)
+        return
+    }
+    setTimeout(function(){
+        $(".aboutWeather").fadeIn("slow")
+         },500)
+    $("#errorMessage").fadeOut("slow")
+
+    // $(".aboutWeather").removeClass("d-none")
+    // $("#errorMessage").addClass("d-none")
+
+ const {weather:[{main}],main:{temp,humidity},wind:{speed} ,sys:{country},name } = weather
+// console.log(main,description,temp,speed,country,name);
+$(".aboutWeather").html(`  <p class=" ">${main}</p>
+<p class="">${temp} C°</p>
+  <p class="">${name}/${country}</p>
+
+ <div class="d-flex justify-content-between"> 
+  <p class="h5">Humidity: ${humidity}%</p>
+  <p class="h5"> Wind speed: ${speed}m/s</p>
+ </div>`)
 $(".content").removeClass("d-none")
 
 }
+});
